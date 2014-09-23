d5a2db10f2ec59b9f68ef26c21505b87a4144be0
vcavallo <vcavallo@gmail.com>
2014-09-22 23:28:13 -0400

    Initial commit of skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Egna::Application.config.secret_key_base = '327b6e0eb9d461081845be80c3f0ce05b7d2ad6c0a0bfadd04954ff5f234fd2e13bdce45f9dba2edc2003df971e73ffc60f15e0915fbd3a616476c593bc0e85e'
